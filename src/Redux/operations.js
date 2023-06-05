ab40e3bc6b6b9959d4ebc5432576c32133528eef
Olena Korenieva <olena.korenieva@gmail.com>
2023-06-05 10:44:50 +0200

    fixed routes

--- src/Redux/operations.js
+++ src/Redux/operations.js
@@ -5,7 +5,8 @@ axios.defaults.baseURL = "https://646241e77a9eead6faca9048.mockapi.io";
 export const getUsers = createAsyncThunk("users/get", async (_, thunkAPI) => {
   try {
     const { data } = await axios.get("/users");
-    return data.sort((b, a) => a.tweets - b.tweets);
+    return data;
+    // .sort((b, a) => a.tweets - b.tweets)
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
